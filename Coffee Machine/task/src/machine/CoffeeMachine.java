5371dfa3d6e31de7d58c309c199455dda3de3ff5
Jon Waters <waters1228@gmail.com>
2019-07-21 12:51:24 -0400

    Implement Coffee Machine stage 3

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,20 +1,57 @@
 package machine;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        int waterPerCupCoffee = 200; // milileters
-        int milkPerCupCoffee  = 50; // milileters
-        int beansPerCupCoffee = 15; // grams
-        int numCupsRequested;
         Scanner scanner = new Scanner(System.in);
+        final int waterPerCupCoffee = 200; // milileters
+        final int milkPerCupCoffee  = 50; // milileters
+        final int beansPerCupCoffee = 15; // grams
+        int waterAvailable;
+        int milkAvailable;
+        int beansAvailable;
+        int cupsRequested;
+        int waterForRequest;
+        int milkForRequest;
+        int beansForRequest;
+        int cupsCoffeePossible;
+        int cupsPerIngredient[] = new int[3];
+        int cupsRemaining;
 
-        System.out.print("Enter how many cups of coffee you will need: ");
-        numCupsRequested = scanner.nextInt();
 
-        System.out.println("\nFor" + numCupsRequested + " cups of coffee you will need:");
-        System.out.println(waterPerCupCoffee * numCupsRequested + " ml of water");
-        System.out.println(milkPerCupCoffee * numCupsRequested + " ml of milk");
-        System.out.println(beansPerCupCoffee * numCupsRequested + " g of coffee beans");
+        System.out.println("Enter how many ml of water the coffee machine has:");
+        waterAvailable = scanner.nextInt();
+
+        System.out.println("Enter how many ml of milk the coffee machine has:");
+        milkAvailable = scanner.nextInt();
+
+        System.out.println("Enter how many grams of coffee beans the coffee machine has:");
+        beansAvailable = scanner.nextInt();
+
+        System.out.println("Enter how many cups of coffee you will need:");
+        cupsRequested = scanner.nextInt();
+
+        waterForRequest = waterPerCupCoffee * cupsRequested;
+        milkForRequest = milkPerCupCoffee * cupsRequested;
+        beansForRequest = beansPerCupCoffee * cupsRequested;
+
+        cupsPerIngredient[0] = waterAvailable / waterPerCupCoffee;
+        cupsPerIngredient[1] = milkAvailable / milkPerCupCoffee;
+        cupsPerIngredient[2] = beansAvailable / beansPerCupCoffee;
+        Arrays.sort(cupsPerIngredient);
+
+        cupsCoffeePossible = cupsPerIngredient[0];
+        cupsRemaining = cupsCoffeePossible - cupsRequested;
+
+        if (cupsCoffeePossible == cupsRequested) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsCoffeePossible > cupsRequested) {
+            System.out.println("Yes, I can make that amount of coffee ( and even " +
+                                cupsRemaining + " more than that)" );
+        } else {
+            System.out.println("No, I can make only" + cupsCoffeePossible +
+                    " cups(s) of coffee");
+        }
     }
 }
