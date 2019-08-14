189b8a0cc0e17ec5c5d4bd9c2776e97ee79a9844
Jon Waters <waters1228@gmail.com>
2019-08-14 14:06:14 -0400

    Implement Coffee Machine stage 5

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -14,43 +14,85 @@ public class CoffeeMachine {
         System.out.println(machineResources.get("milk") + " of milk");
         System.out.println(machineResources.get("beans") + " of coffee beans");
         System.out.println(machineResources.get("cups") + " of disposable cups");
-        System.out.println(machineResources.get("money") + " of money\n");
+        System.out.println("$" + machineResources.get("money") + " of money");
     }
 
     private static String getUserAction() {
-        System.out.print("Write action (buy, fill, take): ");
+        System.out.print("\nWrite action (buy, fill, take, remaining, exit): ");
         String userInput = scanner.nextLine();
         return userInput;
     }
 
+    private static boolean haveResources( HashMap<String, Integer> coffeeType) {
+        boolean enoughResources = true;
+        int water = machineResources.get("water") - coffeeType.get("water");
+        int beans = machineResources.get("beans") - coffeeType.get("beans");
+        int milk = machineResources.get("milk") - coffeeType.get("milk");
+        int cups = machineResources.get("cups") - coffeeType.get("cups");
+
+        if (water < 0) {
+            System.out.println("Sorry, not enough water!");
+            enoughResources = false;
+        }
+
+        if (beans < 0) {
+            System.out.println("Sorry, not enough beans!");
+            enoughResources = false;
+        }
+
+        if (milk < 0) {
+            System.out.println("Sorry, not enough milk!");
+            enoughResources = false;
+        }
+
+        if (cups < 0) {
+            System.out.println("Sorry, not enough cups!");
+            enoughResources = false;
+        }
+
+        return enoughResources;
+
+    }
+
     private static void makeCoffee(HashMap<String, Integer> coffeeType) {
-        machineResources.put("water", machineResources.get("water") -
-                coffeeType.get("water"));
-        machineResources.put("beans", machineResources.get("beans") -
-                coffeeType.get("beans"));
-        machineResources.put("milk", machineResources.get("milk") -
-                coffeeType.get("milk"));
-        machineResources.put("money", machineResources.get("money") +
-                coffeeType.get("money"));
-        machineResources.put("cups", machineResources.get("cups") -
-                coffeeType.get("cups"));
+        if (haveResources(coffeeType)) {
+            System.out.println("I have enough resources, making you a coffee!");
+            machineResources.put("water", machineResources.get("water") -
+                    coffeeType.get("water"));
+            machineResources.put("beans", machineResources.get("beans") -
+                    coffeeType.get("beans"));
+            machineResources.put("milk", machineResources.get("milk") -
+                    coffeeType.get("milk"));
+            machineResources.put("money", machineResources.get("money") +
+                    coffeeType.get("money"));
+            machineResources.put("cups", machineResources.get("cups") -
+                    coffeeType.get("cups"));
+        }
     }
 
     private static void buyCoffee() {
-        System.out.print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        int userChoice = scanner.nextInt();
+        do {
+            System.out.print("\nWhat do you want to buy? 1 - espresso, 2 - latte, " +
+                    "3 - cappuccino, back - to main menu: ");
+            String userChoice = scanner.nextLine();
 
-        switch (userChoice) {
-            case 1:
-                makeCoffee(espressoResources);
-                break;
-            case 2:
-                makeCoffee(latteResources);
-                break;
-            case 3:
-                makeCoffee(cappuccinoResources);
-                break;
-        }
+            switch (userChoice) {
+                case "1":
+                    makeCoffee(espressoResources);
+                    return;
+                case "2":
+                    makeCoffee(latteResources);
+                    return;
+                case "3":
+                    makeCoffee(cappuccinoResources);
+                    return;
+                case "back":
+                    return;
+                default:
+                    System.out.println("\nIncorrect Input! Please try again.");
+                    break;
+            }
+        } while (true);
     }
 
     private static void fillMachine() {
@@ -63,6 +105,8 @@ public class CoffeeMachine {
         System.out.print("Write how many disposable cups of do you want to add: ");
         int additionalCups = scanner.nextInt();
 
+        scanner.nextLine();
+
         machineResources.put("water", machineResources.get("water") + additionalWater);
         machineResources.put("milk", machineResources.get("milk") + additionalMilk);
         machineResources.put("beans", machineResources.get("beans") + additionalBeans);
@@ -70,7 +114,7 @@ public class CoffeeMachine {
     }
 
     private static void takeMoney() {
-        System.out.println("I gave you $" + machineResources.get("money") + "\n");
+        System.out.println("\nI gave you $" + machineResources.get("money"));
         machineResources.put("money", 0);
     }
 
@@ -78,7 +122,7 @@ public class CoffeeMachine {
         scanner = new Scanner(System.in);
 
         machineResources = new HashMap<>();
-        machineResources.put("water", 1200); // milliliters
+        machineResources.put("water", 400); // milliliters
         machineResources.put("milk", 540); // milliliters
         machineResources.put("beans", 120); // grams
         machineResources.put("money", 550); // US dollars
@@ -106,22 +150,32 @@ public class CoffeeMachine {
         cappuccinoResources.put("cups", 1); // disposable drinking cups
 
 
-        displayMachineStatus();
+        String userAction = "";
 
-        String userAction = getUserAction();
+        do {
+            userAction = getUserAction();
 
-        switch (userAction) {
-            case "buy":
-                buyCoffee();
-                break;
-            case "fill":
-                fillMachine();
-                break;
-            case "take":
-                takeMoney();
-                break;
-        }
+            switch (userAction) {
+                case "buy":
+                    buyCoffee();
+                    break;
+                case "fill":
+                    fillMachine();
+                    break;
+                case "take":
+                    takeMoney();
+                    break;
+                case "remaining":
+                    displayMachineStatus();
+                    break;
+                case "exit":
+                    System.out.println("\nGoodbye!");
+                    break;
+                default:
+                    System.out.println("\nIncorrect Input! Please try again.");
+                    break;
+            }
 
-        displayMachineStatus();
+        } while (!userAction.equals("exit"));
     }
 }
